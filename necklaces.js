c860a26ba49502de715924e57fcb2820f5ee7158
Adam <50596437+noszczynski@users.noreply.github.com>
2020-05-20 19:16:40 +0200

    Update necklaces.js

--- necklaces.js
+++ necklaces.js
@@ -2,16 +2,20 @@ const A = [5,3,4,0,1,2];
 
 const solution = (A) => {
     let maxNumber = 1;
+    const repetitions = new Set();
     
     for(let i=0; i < A.length; i++) {
-        let nextNumber = i;
-        let currLength = 0;
-        do {
-            nextNumber = A[nextNumber];
-            currLength++;
-        } while (nextNumber !== i);
-        
-        if(currLength > maxNumber) maxNumber = currLength;
+        if(!repetitions.has(i)){
+            let nextNumber = i;
+            let currLength = 0;
+            do {
+                nextNumber = A[nextNumber];
+                currLength++;
+                repetitions.add(nextNumber);
+            } while (nextNumber !== i);
+
+            if(currLength > maxNumber) maxNumber = currLength;
+        }
     }
     
     return maxNumber;
